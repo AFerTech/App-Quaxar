dd0f9f1df8d2205ffca02132c02ddb30fe7f9764
Alexis Fernando <alexis96fer@gmail.com>
2022-03-22 12:05:56 -0600

    primer commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '77557749b1cee6b858672cb6bc0efd2b193fcf40',
+        'name' => 'alexisfernando/clientes-php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alexisfernando/clientes-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '77557749b1cee6b858672cb6bc0efd2b193fcf40',
+            'dev_requirement' => false,
+        ),
+    ),
+);
