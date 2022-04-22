82a28660a54ba61943f074dc94945c87d7556198
Alexis Fernando <alexis96fer@gmail.com>
2022-04-22 17:29:10 -0500

    generacion de token y envio de correo para confirmar cuenta PHPmailer

---
+++ classes/Email.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Classes;
+
+ use PHPMailer\PHPMailer\PHPMailer;
+
+class Email{
+
+    protected $email;
+    protected $nombre;
+    protected $token;
+
+    public function __construct($email, $nombre, $token)
+    {
+        $this->email = $email;
+        $this->nombre = $nombre;
+        $this->token = $token;
+    }
+    public function enviarCorreoConfirmacion(){
+
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = 'f1bef2fef7082b';
+        $mail->Password = '67311a439d1764';
+
+
+        $mail->setFrom('alexis96fer@gmail.com');
+        $mail->addAddress('cuentas@quaxar.com', 'QUAXAR');
+        $mail->Subject =  'Confirmar cuenta';
+
+        $mail->isHTML(TRUE);
+        $mail->CharSet = 'UTF-8';
+
+        $contenido = '<html>';
+        $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has Creado tu cuenta, da click en el enlace para confirmar</p>";
+        $contenido .= "<p>CLICK AQUI: <a href='http://localhost:3000/confirmar?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>";
+        $contenido .= '</html>';
+
+        $mail->Body= $contenido;
+
+        $mail->send();
+
+    }
+}
